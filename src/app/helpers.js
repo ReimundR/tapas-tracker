3cc7e77aafd928d7ee6d2249771032b39712dbf4
Reimund Renner <reimund@siogeen.com>
2025-10-06 03:58:37 +0200

    improved iOS install prompt

--- src/app/helpers.js
+++ src/app/helpers.js
@@ -202,8 +202,11 @@ export const InstallPrompt = ({ t }) => {
       <h4 className="text-m font-bold mb-4">{t('addToHomeScreen')}</h4>
       {isIOS && displayPrompt === 'Safari' ? (
         <p>
-          {t('installIos1')} <span role="img" aria-label="share icon">{' '}⎋{' '}</span>
-          {t('installIos2')} "{t('addToHomeScreen')}" <span role="img" aria-label="plus icon">{' '}➕{' '}</span>.
+          {t('installIos1')}{' '}
+          <svg className="inline-flex" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+            <path d="M0 0h24v24H0V0z" fill="none"/><path d="M16 5l-1.42 1.42-1.59-1.59V16h-1.98V4.83L9.42 6.42 8 5l4-4 4 4zm4 5v11c0 1.1-.9 2-2 2H6c-1.11 0-2-.9-2-2V10c0-1.11.89-2 2-2h3v2H6v11h12V10h-3V8h3c1.1 0 2 .89 2 2z"/>
+          </svg>{' '}
+          {t('installOther2')} "{t('addToHomeScreen')}" <span className="text-2xl" role="img" aria-label="plus icon">{' '}⊞{' '}</span>.
         </p>
       ) : (
         <p>
