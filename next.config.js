4ba227db392409e22b62c122b46f56305fcc7964
Reimund Renner <reimund@siogeen.com>
2025-11-20 02:36:48 +0100

    cache fix?

--- next.config.js
+++ next.config.js
@@ -41,9 +41,10 @@ module.exports = async (phase) => {
     const withSerwist = (await import("@serwist/next")).default({
       // Note: This is only an example. If you use Pages Router,
       // use something else that works, such as "service-worker/index.ts".
-      swSrc: "src/sw.ts",
+      swSrc: "src/app/sw.ts",
       swDest: "public/sw.js",
-      disable: process.env.NODE_ENV !== "production",
+      cacheOnNavigation: true,
+      //disable: process.env.NODE_ENV !== "production",
     });
     return withSerwist(withMDX(nextConfig));
   }
